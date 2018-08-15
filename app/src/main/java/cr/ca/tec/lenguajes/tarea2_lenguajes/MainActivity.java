53b6540c2e625378b27f7687209ced9ef868ee51
Nelson <nlrarojas@hotmail.com>
2018-08-15 11:03:38 -0600

    Creacion del proyecto

---
+++ app/src/main/java/cr/ca/tec/lenguajes/tarea2_lenguajes/MainActivity.java
@@ -0,0 +1,13 @@
+package cr.ca.tec.lenguajes.tarea2_lenguajes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
